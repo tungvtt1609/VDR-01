c6e87542bf0d1139fecaf686f1c8fe274ca276ef
Võ Thanh Tùng <parkhoon1609@gmail.com>
2022-10-10 17:18:50 +0700

    fix code debug for pwm.cpp

--- lib/PWM/pwm.cpp
+++ lib/PWM/pwm.cpp
@@ -34,49 +34,63 @@ void setup_RC(void)
     pinMode(RCPinSide, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(RCPinFWD), PulseTimerFWD, CHANGE);
     attachInterrupt(digitalPinToInterrupt(RCPinSide), PulseTimerSide, CHANGE);
+
+    get_rpm_right_RC();
+    get_rpm_left_RC();
+    mapp(PulseWidthFWD, in_min, in_max, out_min_v, out_max_v);
+    mapp(PulseWidthSide, in_min, in_max, out_min_w, out_max_w);
+    PulseTimerSide();
+    PulseTimerFWD();
 }
 
 void main_pwm(void)
 {
-    float vel_right, vel_left;
+    setup_RC();
 
-    // Velocity Linear
-    if(PulsesFWD < 2000){
-        PulseWidthFWD = PulsesFWD;
+    while (1){
+
+        float vel_right, vel_left;
+
+        // Velocity Linear
+        if(PulsesFWD < 2000){
+            PulseWidthFWD = PulsesFWD;
+        }
+
+        if(abs(PulseWidthFWD - 1495) < 20)
+        {
+            vel_RC_linear = 0.0;
+        } 
+        else 
+        {
+            vel_RC_linear = mapp(PulseWidthFWD, in_min, in_max, out_min_v, out_max_v);
+        }
+
+        // Velocity angular
+        if(PulsesSide < 2000){
+            PulseWidthSide = PulsesSide;
+        }
+
+        if(abs(PulseWidthSide - 1495) < 20)
+        {
+            vel_RC_angular = 0.0;
+        } 
+        else 
+        {
+            vel_RC_angular = mapp(PulseWidthSide, in_min, in_max, out_min_w, out_max_w);
+        }
+
+        vel_right = 0 - get_rpm_right_RC();
+        vel_left  = get_rpm_left_RC();
+
+        Write_Velocity_rpm(Right_Wheel_ID, (int32_t)vel_right);
+        Write_Velocity_rpm(Left_Wheel_ID, (int32_t)vel_left);
+
+        // Serial.print(PulseWidthFWD);
+        // Serial.print(" ");
+        // Serial.println(PulseWidthSide);
+
     }
-
-    if(abs(PulseWidthFWD - 1495) < 20)
-    {
-        vel_RC_linear = 0.0;
-    } 
-    else 
-    {
-        vel_RC_linear = mapp(PulseWidthFWD, in_min, in_max, out_min_v, out_max_v);
-    }
-
-    // Velocity angular
-    if(PulsesSide < 2000){
-        PulseWidthSide = PulsesSide;
-    }
-
-    if(abs(PulseWidthSide - 1495) < 20)
-    {
-        vel_RC_angular = 0.0;
-    } 
-    else 
-    {
-        vel_RC_angular = mapp(PulseWidthSide, in_min, in_max, out_min_w, out_max_w);
-    }
-
-    vel_right = 0 - get_rpm_right_RC();
-    vel_left  = get_rpm_left_RC();
-
-    Write_Velocity_rpm(Right_Wheel_ID, (int32_t)vel_right);
-    Write_Velocity_rpm(Left_Wheel_ID, (int32_t)vel_left);
-
-    // Serial.print(PulseWidthFWD);
-    // Serial.print(" ");
-    // Serial.println(PulseWidthSide);
+    
 }
 
 void PulseTimerFWD(void){
@@ -97,17 +111,15 @@ void PulseTimerSide(void){
 
 float get_rpm_right_RC(){
     float linear_vel_right, angular_vel_right_s, angular_vel_right_mins_RC;
-    linear_vel_right = vel_RC_linear + (vel_RC_angular * DISTANCE_WHEEL) / 2;
-    angular_vel_right_s = linear_vel_right / (DIAMETER_WHEEL / 2); // rad/s
-    angular_vel_right_mins_RC = (angular_vel_right_s * 30) / 2;
+    angular_vel_right_s = (vel_RC_linear + vel_RC_angular * (DISTANCE_WHEEL / 2)) / (DIAMETER_WHEEL / 2); // rad/s
+    angular_vel_right_mins_RC = (angular_vel_right_s / PI) * 30;
     return angular_vel_right_mins_RC;
 }
 
 float get_rpm_left_RC(){
     float linear_vel_left, angular_vel_left_s, angular_vel_left_mins_RC;
-    linear_vel_left = vel_RC_linear - (vel_RC_angular * DISTANCE_WHEEL) / 2;
-    angular_vel_left_s = linear_vel_left / (DIAMETER_WHEEL / 2); // rad/s
-    angular_vel_left_mins_RC = (angular_vel_left_s * 30) / 2;
+    angular_vel_right_s = (vel_RC_linear - vel_RC_angular * (DISTANCE_WHEEL / 2)) / (DIAMETER_WHEEL / 2); // rad/s
+    angular_vel_right_mins_RC = (angular_vel_right_s / PI) * 30;
     return angular_vel_left_mins_RC;
 }
 
