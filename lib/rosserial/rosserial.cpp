abc3cbf7ed45edbcddbd96f410a2715e6e093c64
Võ Thanh Tùng <parkhoon1609@gmail.com>
2022-12-14 00:40:36 +0700

    clean code

--- lib/rosserial/rosserial.cpp
+++ lib/rosserial/rosserial.cpp
@@ -28,18 +28,6 @@ void commandCallback(const geometry_msgs::Twist &cmd_msg)
     g_req_linear_vel_x = cmd_msg.linear.x;
     g_req_linear_vel_y = cmd_msg.linear.y;
     g_req_linear_vel_z = cmd_msg.angular.z;
-    // if (STATE_ROS == false)
-    // {
-    //     g_req_linear_vel_x = 0;
-    //     g_req_linear_vel_y = 0;
-    //     g_req_linear_vel_z = 0;
-    // }
-    // else{
-    //     g_req_linear_vel_x = cmd_msg.linear.x;
-    //     g_req_linear_vel_y = cmd_msg.linear.y;
-    //     g_req_linear_vel_z = cmd_msg.angular.z;
-    // }
-    
 }
 void setup_rosserial(void)
 {
